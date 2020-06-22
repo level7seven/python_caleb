abeb9cd7e902ce4bb6e6a489b1f697a1c591a617
level7seven <level7seven@hotmail.com>
2020-06-22 21:49:52 +0200

    new 4

--- app.py
+++ app.py
@@ -3,3 +3,22 @@ import math
 result = pow(10,3)
 
 print(result)
+
+# lesson 1
+
+age = 2015
+
+print("age", age)
+
+# result of substraction 
+age1 = 10
+
+age_2 = 12
+
+age_of_eppy = age - age1
+
+git push -u origin masterprint("age of eppy", age_of_eppy)
+
+result = pow(10,3)
+
+print(result)
